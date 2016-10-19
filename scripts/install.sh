85df821c59f04632d88b86711ed3ec68c55b8e2a
Kedar <kedar.gn20@gmail.com>
2016-10-18 20:12:30 -0400

    storm detector new repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,4 @@ cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
 mvn -e clean install >> /var/log/tomcat.log
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 
-sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
+sudo sh /usr/local/tomcat7/apache-tomcat-7.0.72/bin/startup.sh >> /var/log/tomcat.log 2>&1 &
