786cfc620ed1ffe84a64c1717fb7cd55d1983b8e
Soumya Achar <smachar4@gmail.com>
2016-10-21 16:54:40 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@ echo 'starting installation process'
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
 sudo docker pull kedargn/stormdetector
-sudo docker run -d -p 9999:8080 --name stormdetector $(sudo docker images | grep kedargn/stormdetector | awk '{print $3}') >> ./log.txt
+sudo docker run -d -p 8080:9999 --name stormdetector $(sudo docker images | grep kedargn/stormdetector | awk '{print $3}') >> ./log.txt
