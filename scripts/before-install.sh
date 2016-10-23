20ce2a9d913ddb5ae29ba4374ae9abf5d1d3426b
Kedar <kedar.gn20@gmail.com>
2016-10-23 06:11:55 -0400

    test env

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,4 +1,4 @@
 sudo yum install -y docker-io
 sudo service docker start
-sudo docker ps -a | grep 'stormdetector' | awk '{print $1}' | xargs --no-run-if-empty docker stop
-sudo docker ps -a | grep 'stormdetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+sudo docker ps -a | grep 'stormdetectortest' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+sudo docker ps -a | grep 'stormdetectortest' | awk '{print $1}' | xargs --no-run-if-empty docker rm
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 echo 'starting installation process'
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
-sudo docker pull kedargn/stormdetector
-sudo docker run -d -p 9999:8080 --name stormdetector $(sudo docker images | grep kedargn/stormdetector | awk '{print $3}') >> ./log.txt
+sudo docker pull kedargn/stormdetectortest
+sudo docker run -d -p 9999:8080 --name stormdetectortest $(sudo docker images | grep kedargn/stormdetectortest | awk '{print $3}') >> ./log.txt
